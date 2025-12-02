e4263303c49cb297fc4bb71cdb363d6e585547c0
Marcin Olko <molko@google.com>
2025-12-02 17:21:47 +0000

    Added GRPC sync with basic tests
    Signed-off-by: Marcin Olko <molko@google.com>

---
+++ providers/flagd/tests/smoke/sync.cpp
@@ -0,0 +1,27 @@
+#include "flagd/sync.h"
+
+#include <iostream>
+#include <thread>
+
+#include "flagd/configuration.h"
+
+int main() {
+  flagd::GrpcSync test = flagd::GrpcSync(flagd::FlagdProviderConfig());
+
+  test.Init(openfeature::EvaluationContext()).IgnoreError();
+
+  for (int i = 0; i < 20; i++) {
+    auto config = test.GetFlags();
+
+    if (config->contains("myBoolFlag")) {
+      std::cout << "Flag found!" << std::endl;
+    } else {
+      std::cout << "Flag not found (yet)." << std::endl;
+    }
+
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+  }
+
+  test.Shutdown().IgnoreError();
+  return 0;
+}
